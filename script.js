f481b70193d38d0bc8ba6300b48a32e832fb52b0
Anna Kazantseva <annetastara@gmail.com>
2020-01-19 16:46:33 +0200

    fix

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 let clickButton = document.querySelector('#button_click');
 let blockSocialButtons = document.querySelector('.social__buttons');
+let show = document.querySelector('.show');
+let messenger = document.querySelector('.messenger');
 
 clickButton.addEventListener('click', function(){
-    if(getComputedStyle(blockSocialButtons).right === '-166px') {
-        blockSocialButtons.style.display = 'block';
+    if(getComputedStyle(blockSocialButtons).right === '-40px') {
+        blockSocialButtons.style.visibility = 'visible';
         blockSocialButtons.style.right = '46' + 'px';
         blockSocialButtons.style.opacity = '1';
     } else {
-        blockSocialButtons.style.display = 'none';
-        blockSocialButtons.style.right = '-166' + 'px';
-        blockSocialButtons.style.opacity = '0'
+        blockSocialButtons.style.visibility  = 'hidden';
+        blockSocialButtons.style.right = '-40' + 'px';
+        // blockSocialButtons.style.opacity = '0';
+        
+        
+    
     }
 } )
+
+
 
 var where = document.referrer;
 if (where==0) console.log("Посетитель пришел с <b>пустой страницы</b>")
